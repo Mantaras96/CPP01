68f7c13a3129b707f99f757a90c1da28601d75ef
Mantaras96 <albert.mantaras@gmail.com>
2022-06-19 10:33:59 +0200

    Primer commit ex00

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie( std::string name ): name(name)
+{
+}
+
+void Zombie::set_name(const std::string name) {
+    this->name = name;
+}
+
+void Zombie::announce(void) {
+    std::cout << this->name << " BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+std::string Zombie::get_name( void ) const{
+    return (this->name);
+}
